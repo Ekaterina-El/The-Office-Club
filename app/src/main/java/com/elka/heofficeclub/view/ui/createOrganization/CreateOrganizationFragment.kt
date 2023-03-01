08bd4d0aa2943992eb7ab5796119d2168dbde41f
EkaterinaEl <hackerrussia@yandex.ru>
2023-03-01 20:05:32 +0500

    Created create_organization_fragment and added navigation to it

--- app/src/main/java/com/elka/heofficeclub/view/ui/createOrganization/CreateOrganizationFragment.kt
+++ app/src/main/java/com/elka/heofficeclub/view/ui/createOrganization/CreateOrganizationFragment.kt
@@ -1,36 +1,38 @@
-package com.elka.heofficeclub.view.ui.welcomeScreen
+package com.elka.heofficeclub.view.ui.createOrganization
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.elka.heofficeclub.R
+import com.elka.heofficeclub.databinding.CreateOrganizationFragmentBinding
 import com.elka.heofficeclub.databinding.WelcomeFragmentBinding
 import com.elka.heofficeclub.view.ui.BaseFragment
 
-class WelcomeFragment: BaseFragment() {
-    private lateinit var binding: WelcomeFragmentBinding
+class CreateOrganizationFragment: BaseFragment() {
+    private lateinit var binding: CreateOrganizationFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = WelcomeFragmentBinding.inflate(layoutInflater, container, false)
+        binding = CreateOrganizationFragmentBinding.inflate(layoutInflater, container, false)
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
-            master = this@WelcomeFragment
+            master = this@CreateOrganizationFragment
         }
 
         return binding.root
     }
 
-    fun goAuth() {
-        navController.navigate(R.id.action_welcomeFragment_to_authFragment)
+    fun goBack() {
+        navController.popBackStack()
     }
 
-    fun goCreateOrganization() {
-        navController.navigate(R.id.action_welcomeFragment_to_createOrganizationFragment)
+    fun tryRegistration() {
+        Toast.makeText(requireContext(), "Registration", Toast.LENGTH_SHORT).show()
     }
 }

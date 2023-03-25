f1967e7c7ca8e232b1ee3e0c658281b49d037448
Ekaterina El <hackerrussia@yandex.ru>
2023-03-25 18:27:44 +0500

    Set position and division of employer

--- app/src/main/java/com/elka/heofficeclub/viewModel/BaseViewModelEmployer.kt
+++ app/src/main/java/com/elka/heofficeclub/viewModel/BaseViewModelEmployer.kt
@@ -595,10 +595,14 @@ abstract class BaseViewModelEmployer(application: Application) : BaseViewModel(a
 
     moreInform.value = T2.moreInform
 
-//    val T1 = employer.T1
     // TODO: require get T1
     // TODO: require deny change T1 (fields and dates)
     // TODO: fix bug "Кем выдан" (паспорт) == название оранизации
+    _selectedDivision = employer?.divisionLocal ?: Division(name = "-")
+    _selectedPosition = employer?.positionLocal ?: OrganizationPosition(name = "-")
+
+    //    val T1 = employer.T1Local
+
     conditionOfWork.value = ""
     natureOfWork.value = ""
     premium.value = ""

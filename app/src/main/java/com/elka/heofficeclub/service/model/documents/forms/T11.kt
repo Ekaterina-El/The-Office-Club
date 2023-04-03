07fc3769988d24d84db11e06aae913a6a1b38dfc
EkaterinaEl <hackerrussia@yandex.ru>
2023-04-03 18:53:06 +0500

    Created class to form T-11

---
+++ app/src/main/java/com/elka/heofficeclub/service/model/documents/forms/T11.kt
@@ -0,0 +1,39 @@
+package com.elka.heofficeclub.service.model.documents.forms
+
+import com.elka.heofficeclub.other.documents.FormType
+import com.elka.heofficeclub.other.getDaysBetween
+import com.elka.heofficeclub.service.model.Division
+import com.elka.heofficeclub.service.model.Employer
+import com.elka.heofficeclub.service.model.Organization
+import com.elka.heofficeclub.service.model.OrganizationPosition
+import java.util.*
+
+class T11(
+  override val type: FormType = FormType.T6,
+  override var fileUrl: String? = null,
+  override var number: Int = 0,
+  override val dataCreated: Date = Calendar.getInstance().time,
+  val organization: Organization? = null,
+
+  val employer: Employer? = null,
+  val division: Division? = null,
+  val position: OrganizationPosition? = null,
+
+  val description: String = "",
+  val giftType: String = "",
+
+  val sum: Double = 0.0,
+  val sumS: String = "",
+
+  val reason: String = ""
+
+) : DocForm(
+  fileUrl = fileUrl,
+  number = number,
+  orgId = organization!!.id,
+  orgName = organization.fullName,
+  codeOKPO = organization.okpo,
+  dataCreated = dataCreated,
+  type = type
+) {
+}
